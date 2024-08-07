a67ea7d17f59f0a7e426d76c9945fad2de9c9aa4
Jamie Thompson <jamie@growandconnecttherapy.com>
2024-08-07 09:37:10 -0600

    Reduce contact page code.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { FC } from "react";
+import type { FC } from "react";
 import { Resend } from "resend";
 import { z } from "zod";
 
@@ -19,6 +19,10 @@ export default function Page() {
       message: z.string(),
     });
 
+    const parsedFormData = EmailProps.parse(
+      Object.fromEntries(formData.entries()),
+    );
+
     const Email: FC<z.infer<typeof EmailProps>> = (props) => (
       <div>
         <h1>Initial Contact</h1>
@@ -27,58 +31,37 @@ export default function Page() {
       </div>
     );
 
-    const parsedFormData = EmailProps.safeParse({
-      email: formData.get("email"),
-      message: formData.get("message"),
-    });
-
-    if (!parsedFormData.success) {
-      return {
-        errors: parsedFormData.error.flatten().fieldErrors,
-      };
-    }
-
     const resend = new Resend(process.env["RESEND_API_KEY"]);
     const response = await resend.emails.send({
       from: "Incoming <jamie@growandconnecttherapy.com>",
       to: ["Jamie Thompson <jamie@growandconnecttherapy.com>"],
       subject: "Testing Email",
-      react: Email(parsedFormData.data),
+      react: Email(parsedFormData),
     });
 
     if (response.error) {
-      return {
-        errors: response.error,
-      };
+      return { errors: response.error };
+    } else {
+      return { success: true };
     }
-
-    return {
-      success: true,
-    };
   }
 
   return (
-    <MainHeader>
-      <SlideHeader>
-        <h1>Contact</h1>
-        <h2>Contact</h2>
-        <h3>Contact</h3>
-        <h4>Contact</h4>
-        <h5>Contact</h5>
-        <h6>Contact</h6>
-        <p className="theme-text-large">How about that?</p>
-        <p>How about that?</p>
-        <p className="theme-text-label">How about that?</p>
-        {/* This seems to work in next.js, but eslint complans... */}
+    <>
+      <MainHeader>
+        <SlideHeader>
+          <h1>Contact</h1>
+        </SlideHeader>
+        {/* eslint doesn't like next.js's async action functionality, but it works */}
         {/* eslint-disable-next-line */}
         <form action={sendEmail}>
           <label htmlFor="email">Email:</label>
-          <input type="text" id="email" name="email" />
+          <input required type="email" id="email" name="email" />
           <label htmlFor="messager">Message:</label>
-          <input type="text" id="message" name="message" />
+          <input required type="text" id="message" name="message" />
           <button type="submit">Submit</button>
         </form>
-      </SlideHeader>
-    </MainHeader>
+      </MainHeader>
+    </>
   );
 }
